1581be3a64f26bd0d53f8cb608f721df5ce948a8
margdaly <margaret.daly@gmail.com>
2023-01-05 15:09:53 -0500

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,16 +11,16 @@
 
 
 "Hello World".include?("Hello")
-
+- puts "The include? method is called on the string object 'Hello World' to determine if it contains the string argument defined as 'Hello'.When called it should return a boolean value (true or false) in this case it should return 'true'"
 
 "Hello World".end_with?("Hello")
-
+- puts "The .end_with? method is being called on the string object 'Hello World'to determine if it ends with the string argument 'Hello'. When called it should return a boolean value of 'false'"
 
 "Hello World".end_with?("rld")
-
+- puts "Here the .end_with? method is called on the same string objest as before but using a different argument. This time when it's called it will return a boolean value of 'tru' because the strong object ended with 'rld'"
 
 12.even?
-
+- puts "The .even? method is used on an integer and when called returns a boolean response of true (if the integer was even) and false (if the integer was odd). There must be an integer for it to work."
 
 18.next
 
