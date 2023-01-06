10ae0cc30ddcfc36edc76cc3b5ad7cfec3650bdb
margdaly <margaret.daly@gmail.com>
2023-01-05 19:55:00 -0500

    Make changes to the last two responses

--- define_and_id.rb
+++ define_and_id.rb
@@ -20,9 +20,9 @@
 - puts "Here the .end_with? method is called on the same string objest as before but using a different argument. This time when it's called it will return a boolean value of 'tru' because the strong object ended with 'rld'"
 
 12.even?
-- puts "The .even? method is used on an integer and when called returns a boolean response of true (if the integer was even) and false (if the integer was odd). There must be an integer for it to work."
+- puts "The .even? method is used on an integer 12 and when called returns a boolean response of true (if the integer was even) and false (if the integer was odd). There must be an integer for it to work. Here it would return the output of true."
 
 18.next
-- puts "The .next is called on an integer and returns the integer but adds 1. For example 72.next would return the output of 73."
+- puts "The .next is called on the integer 18 and returns the integer but adds 1. For example 18.next would return the output of 19."
 
 
